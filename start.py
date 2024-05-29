d1e164d5513244a5bc3073517c4ae14a0ca10aae
hermesgido <hermessgido@gmail.com>
2024-05-29 16:22:46 +0300

    initial commit

---
+++ start.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+RECIPIENT_WAID = os.getenv("RECIPIENT_WAID")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+VERSION = os.getenv("VERSION")
+
+APP_ID = os.getenv("APP_ID")
+# APP_SECRET = os.getenv("APP_SECRET")
+
+def send_whatsapp_message():
+    # url = f"https://graph.facebook.com/v18.0/152251277978639/messages"
+    url = f"https://graph.facebook.com/{VERSION}/{PHONE_NUMBER_ID}/messages"
+    print(url)
+    headers = {
+        "Authorization": "Bearer " + ACCESS_TOKEN,
+        "Content-Type": "application/json",
+    }
+    data = {
+        "messaging_product": "whatsapp",
+        "to": RECIPIENT_WAID,
+        "type": "template",
+        "template": {"name": "hello_world", "language": {"code": "en_US"}},
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response
+
+
+# Call the function
+response = send_whatsapp_message()
+print(response.status_code)
+print(response.json())
